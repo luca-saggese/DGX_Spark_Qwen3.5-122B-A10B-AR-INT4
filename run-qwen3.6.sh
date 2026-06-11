771a5ed60d9a9b19a85b3928f8a37248fe499e11
Luca Saggese <luca.saggese@gmail.com>
2026-06-11 09:16:21 +0200

    wip

--- run-qwen3.6.sh
+++ run-qwen3.6.sh
@@ -14,23 +14,14 @@ docker run -ti --rm --name vllm-server \
     -e VLLM_ALLOW_LONG_MAX_MODEL_LEN=1 \
     vllm-qwen35-v2 \
     serve ${MODEL} \
-    --disable-custom-all-reduce \
-    --attention-backend FLASHINFER \
-    --max-model-len 524288 \
-    --tensor-parallel-size 2 \
-    --max-num-seqs 11 \
-    --enable-chunked-prefill \
-    --enable-prefix-caching \
-    --max-num-batched-tokens 16384 \
-    --gpu-memory-utilization 0.926 \
-    --tool-call-parser qwen3_coder \
+    -served-model-name qwen/qwen3.5 \
+    --max-num-batched-tokens 32768 \
+    --gpu-memory-utilization 0.88 \
+    --port 8000 \
+    --host 0.0.0.0 \
+    --max-model-len 262144 \
     --reasoning-parser qwen3 \
     --enable-auto-tool-choice \
-    --host 0.0.0.0 \
-    --port ${VLLM_PORT} \
-    --dtype auto \
-    --tokenizer-mode auto \
-    --limit-mm-per-prompt '{"image":5, "video":0}' \
-    --speculative-config '{"method":"mtp", "num_speculative_tokens":2}' \
-    --hf-overrides '{"text_config": {"rope_parameters": {"mrope_interleaved": true, "mrope_section": [11, 11, 10], "rope_type": "yarn", "rope_theta": 10000000, "partial_rotary_factor": 0.25, "factor": 4.0, "original_max_position_embeddings": 262144}}}' \
-    --served-model-name qwen/qwen3.5 
+    --tool-call-parser qwen3_coder \
+    --attention-backend FLASHINFER \
+    --speculative-config '{"method":"mtp","num_speculative_tokens":2}'
